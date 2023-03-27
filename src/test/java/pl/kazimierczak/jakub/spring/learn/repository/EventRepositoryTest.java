033c2fa0a423848084cbb9068c4185345758ba35
kamilblack <122618026+kamilblack@users.noreply.github.com>
2023-03-27 16:14:32 +0200

    Zadanie domowe - wersja robocza

--- src/test/java/pl/kazimierczak/jakub/spring/learn/repository/EventRepositoryTest.java
+++ src/test/java/pl/kazimierczak/jakub/spring/learn/repository/EventRepositoryTest.java
@@ -1,9 +1,14 @@
 package pl.kazimierczak.jakub.spring.learn.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.kazimierczak.jakub.spring.learn.repository.entity.DashboardEntity;
 import pl.kazimierczak.jakub.spring.learn.repository.entity.EventEntity;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +19,25 @@ class EventRepositoryTest {
     EventRepository eventRepository;
 
     @Test
-    void test(){
+    void create(){
+        // Given
+
+
+        // When
+        EventEntity entity = new EventEntity();
+        entity.setCity("Berlin");
+        entity.setName("Cars");
+        eventRepository.save(entity);
+
+        EventEntity entityCompare = eventRepository.findById(entity.getId()).get();
+
+        // Then
+        Assertions.assertThat(entityCompare).isEqualTo(entity);
+
+    }
+
+    @Test
+    void update(){
         // Given
 
 
@@ -23,7 +46,48 @@ class EventRepositoryTest {
         entity.setId(1L);
         entity.setCity("London");
         entity.setName("Human");
-        eventRepository.save(entity);
+//        eventRepository.save(entity);
+//        eventRepository.persist(entity);
+        // Then
+
+
+    }
+
+    @Test
+    void readById(){
+        // Given
+
+
+        // When
+        EventEntity entity = new EventEntity();
+
+
+        // Then
+
+
+    }
+
+    @Test
+    void deleteById(){
+        // Given
+
+
+        // When
+
+
+        // Then
+
+
+    }
+
+    @Test
+    void findAll(){
+        // Given
+
+
+        // When
+        List<EventEntity> events = eventRepository.findAll();
+        System.out.println(events);
 
         // Then
 
